4c8db52e4c33ca6aa7117b311bd167a0fcd5b8a6
Saxo_Broko <saxonbrooker@gmail.com>
2020-07-28 01:09:56 +1000

    trying to change installer

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
     message: process.platform === 'win32' ? releaseNotes : releaseName,
     detail: 'A new version has been downloaded. Restart the application to apply the updates.'
   }
+autoUpdater.on('error', message => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
 
   dialog.showMessageBox(dialogOpts).then((returnValue) => {
     if (returnValue.response === 0) autoUpdater.quitAndInstall()
